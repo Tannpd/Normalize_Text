d77ea9f3949920d633a7ed086f2e630eb7e713cf
DELL <DELL@10.12.15.170>
2023-09-26 14:14:39 +0700

    Normalize_Text

---
+++ src/View/Main.java
@@ -0,0 +1,14 @@
+package View;
+
+import controller.NormalizeText;
+import model.TextNormalizerModel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TextNormalizerModel model = new TextNormalizerModel();
+        
+        new NormalizeText(model).run();
+    }
+    
+}
